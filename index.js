357f0c831b7176f31168d230eb93bf2f1a8f64e8
Wafiq Mohamed <wafiqmohameddoo@gmail.com>
2024-03-30 14:12:59 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod;
+let num3 = 40;
+let num4 = 10;
+mod = num3/num4;
+
+let max;
+let set =([1,2,3,10,20])
+max = Math.max(1, 2, 3, 10, 20);
